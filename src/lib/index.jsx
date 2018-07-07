670740e28d31ad1a6faa4d65bec7d4b2442330a4
rzkhosroshahi <rzkhosroshahi@gmail.com>
2018-07-07 19:49:27 +0430

    use dynamic import for retrieving icons path

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import IconsPaths from './icons';
 
-const SvgFontIcons = (props) => {
-  const {
-    name,
-    size,
-    className,
-    sets,
-  } = props;
+class SvgFontIcons extends Component {
+  constructor() {
+    super();
 
-  const icon = IconsPaths[sets][name];
-  const customClassName = !className ? 'svg__icon' : `svg__icon ${className}`;
-
-  if (!icon) {
-    return null;
+    this.state = {
+      IconsPaths: null,
+    };
   }
 
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      width={size}
-      height={size}
-      className={customClassName}
-      viewBox={icon.view}
-    {...props}
-    >
-      <path d={icon.d} />
-    </svg>
-  );
-};
+  UNSAFE_componentWillMount() {
+    const { sets } = this.props;
+
+    import(`./icons/${sets}`)
+      .then((blob) => {
+        this.setState({
+          IconsPaths: blob.default,
+        });
+      });
+  }
+
+  render() {
+    const {
+      name,
+      size,
+      className,
+    } = this.props;
+
+    const icon = this.state.IconsPaths && this.state.IconsPaths[name];
+    const customClassName = !className ? 'svg__icon' : `svg__icon ${className}`;
+
+    if (!icon) {
+      return null;
+    }
+
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        width={size}
+        height={size}
+        className={customClassName}
+        viewBox={icon.view}
+      {...this.props}
+      >
+        <path d={icon.d} />
+      </svg>
+    );
+  }
+}
 
 SvgFontIcons.propTypes = {
   name: PropTypes.string.isRequired,
